c8e94463415be38cc1527177ecd5e1d91c5989d1
Ryan Williams <git@ryan.nz>
2021-12-05 23:17:01 +1300

    Cleanup sun fragment shader

--- src/shaders/sun.frag.glsl
+++ src/shaders/sun.frag.glsl
@@ -14,31 +14,25 @@ float map(float s, float a1, float a2, float b1, float b2) {
 
 void main(void) {
 	float len = length(vTexCoord.xy);
-	float grad = vTexCoord.y;
 	if (len < 1.0) {
+		float grad = vTexCoord.y;
+		float sun = map(grad, 0.0, 1.0, 0.3, 1.3);
+		vec3 color = mix(SUN_BOTTOM, SUN_TOP, sun);
 		float alpha = 1.0 - smoothstep(0.0, 1.0, map(len, 0.99, 1.0, 0.0, 1.0));
+		gl_FragColor = vec4(color, alpha);
 		// Some stripes
-		bool hit = false;
 		for (int i = 0; i < STRIPE_COUNT; i++) {
 			float stripe = (grad + 1.0) / 2.0;
 			float stripeOffset = uTime / 20000.0;
 			stripeOffset += float(i) * 0.1;
 			stripeOffset = mod(stripeOffset, 1.0);
-			stripeOffset = max(0.0, stripeOffset);
-			stripeOffset = min(1.0, stripeOffset);
 			stripeOffset = 1.0 - pow(1.0 - stripeOffset, 3.0);
 			float thickness = (1.0 - stripeOffset) * 0.07;
 			thickness = ceil((thickness * 3000.0)+0.1) / 3000.0;
-			if (!hit && stripe >= stripeOffset - thickness && stripe <= stripeOffset + thickness) {
+			if (stripe >= stripeOffset - thickness && stripe <= stripeOffset + thickness) {
 				gl_FragColor = STRIPE_COLOR;
-				hit = true;
 				break;
 			}
-		}
-		if (!hit) {
-			grad = map(vTexCoord.y, 0.0, 1.0, 0.3, 1.3);
-			vec3 color = mix(SUN_BOTTOM, SUN_TOP, grad);
-			gl_FragColor = vec4(color, alpha);
 		}
 	}
 	else {
